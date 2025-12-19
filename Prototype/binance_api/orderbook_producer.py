826e41b8cadeea588c644ed79bd96c3803a26003
madmoon <madmoontokyo@gmail.com>
2025-12-20 06:13:54 +0700

    kafka orderbook

--- Prototype/binance_api/orderbook_producer.py
+++ Prototype/binance_api/orderbook_producer.py
@@ -10,7 +10,8 @@ endpoint = "/fapi/v1/depth"
 symbol = "BTCUSDT"
 
 def main():
-    producer = KafkaProducer(bootstrap_servers='localhost:9092')
+    producer = KafkaProducer(bootstrap_servers='localhost:9092',
+                             value_serializer=lambda v: json.dumps(v).encode('utf-8'))
 
     url = f"{base_url}{endpoint}"
     params = {
@@ -22,10 +23,14 @@ def main():
         response = requests.get(url, params=params)
         if response.status_code == 200:
             data = response.json()
-            producer.send(TOPIC, data)
-            producer.flush()
+            future = producer.send(TOPIC, key=symbol.encode(), value=data)
+            record_metadata = future.get(timeout=10)
+
+            print(f"Message sent! {record_metadata.partition}")
         else:
             print(f"ERROR! Status code: {response.status_code}")
+        
+        producer.flush()
         time.sleep(1.5)
 
 if __name__ == '__main__':
