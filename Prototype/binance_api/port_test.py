34e364161abe288a54acd5af444fde72294cb89d
madmoon <madmoontokyo@gmail.com>
2025-12-20 14:12:17 +0700

    clean up

--- Prototype/binance_api/port_test.py
+++
@@ -1,9 +0,0 @@
-import socket
-
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-result = sock.connect_ex(('127.0.0.1', 9092))
-if result == 0:
-   print("Cổng 9092 đang MỞ. Kafka đã sẵn sàng!")
-else:
-   print("Cổng 9092 đang ĐÓNG. Kiểm tra lại Docker nhé.")
-sock.close()
